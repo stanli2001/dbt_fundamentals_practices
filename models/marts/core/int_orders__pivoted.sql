d3cb83e163d1978f102d5606504eaacf35842d7b
stanli2001 <stanli2001@gmail.com>
2024-01-23 10:20:32 +1100

    added another jinjia

--- models/marts/core/int_orders__pivoted.sql
+++ models/marts/core/int_orders__pivoted.sql
@@ -1,19 +1,19 @@
+{%- set order_status = ['return_pending','returned','completed','placed','shipped'] -%}
 
-{%-  set payment_method_list = ['bank_transfer','coupon','credit_card','gift_card'] -%}
-
-with payments as (
-    select * from {{ ref('stg_payments') }}
+with orders as (
+    SELECT status,count(*) as cnt
+    from {{ ref('stg_orders') }}
+    group by status  
 ),
 
 pivoted as (
     select 
-        order_id,
-        {%- for payment in payment_method_list %}            
-            sum(case when payment_method='{{ payment }}' then amount else 0 end) as {{ payment }}_amount 
-            {%- if not loop.last -%} , {%- endif -%}
-        {% endfor %}        
-    from payments
-    where status = 'success'
-    group by 1
+           {%- for status in order_status %}
+              sum(case when status = '{{status}}' then cnt else 0 end) as {{status}}_count
+              {%- if not loop.last -%} , {%- endif -%}
+           {% endfor %}
+    from orders    
 )
+
 select * from pivoted
+
