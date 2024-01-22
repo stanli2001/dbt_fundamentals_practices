2bc703e8b137a3b1997ffc57abf60daddcde13c7
stanli2001 <stanli2001@gmail.com>
2024-01-23 10:56:45 +1100

    added macro cents_to_dollars

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -2,8 +2,8 @@ with payments as (
     SELECT id, 
            orderid as order_id,
            paymentmethod as payment_method, status, 
-           -- amount is stored in cents, convert it to dollars
-           amount/100 amount, 
+           -- amount is stored in cents, convert it to dollars    
+           {{ cents_to_dollars('amount') }} as amount,
            created
     FROM {{ source('jaffle_shop','payments') }}
     where status='success'
